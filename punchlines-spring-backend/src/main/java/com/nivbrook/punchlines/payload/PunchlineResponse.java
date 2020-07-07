5795a05034514d00ddb83f968e32d5fe565d333e
nivbrook <nivbrook@gmail.com>
2020-07-06 17:18:48 -0700

    Added backend for fetching Punchlines

---
+++ punchlines-spring-backend/src/main/java/com/nivbrook/punchlines/payload/PunchlineResponse.java
@@ -0,0 +1,37 @@
+package com.nivbrook.punchlines.payload;
+
+import java.time.Instant;
+
+public class PunchlineResponse {
+	private Long id;
+	private String text;
+	private UserSummary createdBy;
+	private Instant creationDateTime;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public UserSummary getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(UserSummary createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Instant getCreationDateTime() {
+		return creationDateTime;
+	}
+	public void setCreationDateTime(Instant creationDateTime) {
+		this.creationDateTime = creationDateTime;
+	}
+	
+	
+}
